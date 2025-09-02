57e60f1bd173f8b5d956a9f4c12ef902e01f4ffc
AkbarZeynalli <akbarhz-af106@code.edu.az>
2025-09-03 01:26:47 +0400

    Sql serverdən məlumatları oxudum,Məlumatı WinForms tətbiqimdə RichTextBox üzərində real-time göstərdim,Eyni məlumatları Excel faylına dinamik şəkildə başlıqlarla birlikdə yazdım,Nəticədə həm tətbiqdə, həm də Excel-də eyni məlumatları idarə etmək mümkün oldu

---
+++ ExcelVTIntegrationProject/ExcelVTIntegrationProject/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ExcelVTIntegrationProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnVTdenOku = new Button();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // btnVTdenOku
+            // 
+            btnVTdenOku.Location = new Point(533, 125);
+            btnVTdenOku.Name = "btnVTdenOku";
+            btnVTdenOku.Size = new Size(171, 83);
+            btnVTdenOku.TabIndex = 0;
+            btnVTdenOku.Text = "Məlumat Bazasından oxu və EXCEL`ə yaz";
+            btnVTdenOku.UseVisualStyleBackColor = true;
+            btnVTdenOku.Click += btnVTdenOku_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(44, 125);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(437, 125);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(btnVTdenOku);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnVTdenOku;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ ExcelVTIntegrationProject/ExcelVTIntegrationProject/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Data.SqlClient; 
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelVTIntegrationProject
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection = new SqlConnection(
+            @"Data Source=DESKTOP-H6VMCSI\SQLEXPRESS;Initial Catalog=ProjectsVT;Integrated Security=True;Trust Server Certificate=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVTdenOku_Click(object sender, EventArgs e)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+
+            Excel.Workbook workbook = excelApp.Workbooks.Add(Type.Missing);
+            Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Sheets[1];
+
+            string[] columnNames = { "PersonalsNo", "Ad", "Soyad", "Rayon", "Şəhər" };
+            Excel.Range range;
+            for (int i = 0; i <columnNames.Length; i++)
+            {
+                 range  = worksheet.Cells[1, i + 1];
+                 range.Value2 = columnNames[i];
+            }
+            try
+            {
+                sqlConnection.Open();
+                string sqlQuery = "Select  PersonalsNo, Ad, Soyad, Rayon, Şəhər from Personal";
+                SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                int row = 2; // Start from the second row, as the first row is for column names
+                while (sqlDataReader.Read())
+                {
+                    string personalsNo = sqlDataReader[0].ToString();
+                    string ad = sqlDataReader[1].ToString();
+                    string soyad = sqlDataReader[2].ToString();
+                    string rayon = sqlDataReader[3].ToString();
+                    string seher = sqlDataReader[4].ToString();
+                    richTextBox1.Text = richTextBox1.Text + personalsNo + " " + ad + " " + soyad + " " + rayon + " " + seher + "\n";
+
+                    range = worksheet.Cells[row, 1];
+                    range.Value2 = personalsNo;
+                    range = worksheet.Cells[row, 2];
+                    range.Value2 = ad;
+                    range = worksheet.Cells[row, 3];
+                    range.Value2 = soyad;
+                    range = worksheet.Cells[row, 4];
+                    range.Value2 = rayon;
+                    range = worksheet.Cells[row, 5];
+                    range.Value2 = seher;
+                    row++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sql Qury sırasında problem oldu, Xəta Kodu:SQLREAD01 \n" + ex.ToString());
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                {
+                sqlConnection.Close();                    
+                }
+            }
+        }
+    }
+}
